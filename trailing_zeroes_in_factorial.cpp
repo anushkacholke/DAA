0e6ae0b29fb7c0ac41ed05450e8b7d2c5147ec1d
Anushka Rahul Cholke <163493588+anushkacholke@users.noreply.github.com>
2024-10-13 14:06:08 +0530

    second commit trailing_zeroes_in_factorial.cpp

--- trailing_zeroes_in_factorial.cpp
+++ trailing_zeroes_in_factorial.cpp
@@ -7,19 +7,42 @@ Input : n = 1 Output : 5
 Input : n = 6 Output : 25*/
 #include<iostream>
 using namespace std;
-int trailing_zeroes(int num)
+int trailing_zero(int num)
 {
     int count=0;
-    for(int i=5;i<=num;i=i*5)
+    for(int i=5;i<num;i=i*5)
     {
-        count=count+num/i;
+        count+=num/i;
     }
-    cout<<"No. of trailing zeroes: "<<count;
+    return count;
+}
+int find_small_number(int n)
+{
+    if(n==0)
+    {
+        return 0; //no trailing zero
+    }
+    int low=0;
+    int high=5*n;  //upper bound trailing 0 must <= 5*n
+    while (low<high)
+    {
+        int mid=(low+high)/2;
+        //check how many trailing 0 in mid
+        if(trailing_zero(mid)>=n)
+        {
+            high=mid;//if it has enough 0 try for smaller num
+        }
+        else{
+            low=mid+1;//otherwise inc lower bound
+        }
+    }
+    return low;//lowest num with atleast n trailing 0
 }
 int main()
 {
-    int num;
-    cout<<"\nEnter number: ";
-    cin>>num;
-    trailing_zeroes(num);
+    int n;
+    cout<<"\nEnter num of trailing zero: ";
+    cin>>n;
+    int res=find_small_number(n);
+    cout<<"\nSamllest num whose factorial has at least "<<n<<" trailing zero is: "<<res;   
 }
